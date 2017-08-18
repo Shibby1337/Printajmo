eea2b7fee44783aa0b9dd51f94d54989a436c0e3
Marino Lukovič <marino.lukovic@student.um.si>
2017-08-18 16:40:51 +0200

    halu

---
+++ Printajmo/Models/Tiskarne.edmx.sql
@@ -0,0 +1,68 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 08/09/2017 23:41:36
+-- Generated from EDMX file: C:\Users\ML\Source\Repos\Printajmo\Printajmo\Models\Tiskarne.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [tiskarne];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+IF OBJECT_ID(N'[dbo].[tiskarne]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[tiskarne];
+GO
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'tiskarne'
+CREATE TABLE [dbo].[tiskarne] (
+    [idtiskarne] int IDENTITY(1,1) NOT NULL,
+    [naziv] varchar(45)  NULL,
+    [a4cb] decimal(10,2)  NULL,
+    [a4barvno] decimal(10,2)  NULL,
+    [a4cboboje] decimal(10,2)  NULL,
+    [a4barvnooboje] decimal(10,2)  NULL,
+    [ulica] varchar(45)  NULL,
+    [postnast] int  NULL,
+    [mesto] varchar(45)  NULL,
+    [email] varchar(45)  NULL,
+    [telefonska] varchar(45)  NULL,
+    [dodatno] varchar(200)  NULL,
+    [longitude] float  NOT NULL,
+    [latitude] float  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [idtiskarne] in table 'tiskarne'
+ALTER TABLE [dbo].[tiskarne]
+ADD CONSTRAINT [PK_tiskarne]
+    PRIMARY KEY CLUSTERED ([idtiskarne] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
