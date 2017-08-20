026c4c3d7f895acc303b538dda26663af604dac9
Shibby <tine.nabernik@gmail.com>
2017-08-20 22:13:10 +0200

    delas?

--- Printajmo/Controllers/HomeController.cs
+++ Printajmo/Controllers/HomeController.cs
@@ -101,17 +101,25 @@ namespace Printajmo.Controllers
 
         public ActionResult AjaxTabela(string sortOrder, string currentFilter, string searchString, int? page)
         {
-            return PartialView("_Tabela", GetModel(sortOrder, currentFilter, searchString, page));
+            return PartialView("_TabelaPartial", GetModel(sortOrder, currentFilter, searchString, page));
         }
-        public ActionResult MojeTiskarne()
+        public ActionResult MojeTiskarne(string selected)
         {
-            return PartialView("_Tabela", GetModelForTiskarna());
+            ViewBag.Selected = selected;
+            return PartialView("_TabelaPartial", GetModelForTiskarna());
         }
         public ActionResult DeleteEntry(int id, string sortOrder, string currentFilter, string searchString, int? page)
         {
             _db.tiskarne.Remove(_db.tiskarne.Find(id));
             _db.SaveChanges();
-            return PartialView("_Tabela", GetModel(sortOrder, currentFilter, searchString, page));
+            if (User.IsInRole("Tiskarna")) {
+                ViewBag.Selected = "true";
+                return PartialView("_TabelaPartial", GetModelForTiskarna());
+            }
+            else
+            {
+                return PartialView("_TabelaPartial", GetModel(sortOrder, currentFilter, searchString, page));
+            }
         }
         public ActionResult EditEntry(int id, string sortOrder, string currentFilter, string searchString, int? page, string naziv, string email, string telefonska, string ulica, string mesto, int postnast, decimal a4cb, decimal a4barvno, decimal a4cboboje, decimal a4boboje)
         {
@@ -128,7 +136,15 @@ namespace Printajmo.Controllers
             tiskarna.a4barvnooboje = a4boboje;
             _db.Entry(tiskarna).State = System.Data.Entity.EntityState.Modified;
             _db.SaveChanges();
-            return PartialView("_Tabela", GetModel(sortOrder, currentFilter, searchString, page));
+            if (User.IsInRole("Tiskarna"))
+            {
+                ViewBag.Selected = "true";
+                return PartialView("_TabelaPartial", GetModelForTiskarna());
+            }
+            else
+            {
+                return PartialView("_TabelaPartial", GetModel(sortOrder, currentFilter, searchString, page));
+            }
         }
 
         public ActionResult AddEntry(string sortOrder, string currentFilter, string searchString, int? page, string naziv, string email, string telefonska, string ulica, string mesto, int postnast, decimal a4cb, decimal a4barvno, decimal a4cboboje, decimal a4boboje)
@@ -147,7 +163,15 @@ namespace Printajmo.Controllers
             tiskarna.lastnik = User.Identity.GetUserId();
             _db.tiskarne.Add(tiskarna);
             _db.SaveChanges();
-            return PartialView("_Tabela", GetModel(sortOrder, currentFilter, searchString, page));
+            if (User.IsInRole("Tiskarna"))
+            {
+                ViewBag.Selected = "true";
+                return PartialView("_TabelaPartial", GetModelForTiskarna());
+            }
+            else
+            {
+                return PartialView("_TabelaPartial", GetModel(sortOrder, currentFilter, searchString, page));
+            }
         }
 
         public ActionResult GetTiskarnaDelete(int id)
@@ -173,7 +197,8 @@ namespace Printajmo.Controllers
         public JsonResult getTiskarneJson() {
             String str= "{\"tiskarne\": [";
             foreach (var item in _db.tiskarne) {
-                str += "{\"lat\":\"" + item.latitude + "\",\"lng\":\"" + item.longitude+"\",\"id\":\""+item.idtiskarne+"\",\"title\":\""+item.naziv+"\"},";
+                if(item.latitude != null || item.longitude != null)
+                    str += "{\"lat\":\"" + item.latitude + "\",\"lng\":\"" + item.longitude+"\",\"id\":\""+item.idtiskarne+"\",\"title\":\""+item.naziv+"\"},";
             }
             str = str.TrimEnd(',') + "]}";
             return Json(str, JsonRequestBehavior.AllowGet);
