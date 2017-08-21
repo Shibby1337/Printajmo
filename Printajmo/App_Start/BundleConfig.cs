3f61c4a7d2644e4e6191150d83ed99c40c5f84f1
Marino Lukovič <marino.lukovic@student.um.si>
2017-08-21 14:42:42 +0200

    asdas

--- Printajmo/App_Start/BundleConfig.cs
+++ Printajmo/App_Start/BundleConfig.cs
@@ -26,6 +26,9 @@ namespace Printajmo
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryunob").Include(
+            "~/Scripts/jquery.unobtrusive*"));
         }
     }
 }
